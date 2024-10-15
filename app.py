829d45b2c2be506beb6a366a9e6683f9bfa1ca10
Ethan Cai <46478203+caiqile@users.noreply.github.com>
2024-10-15 17:03:24 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask,render_template,request
-import google.generativeai as palm
+import google.generativeai as genai
 import random
 import os
+from textblob import TextBlob
 
-api = os.getenv("MAKERSUITE_API_TOKEN") 
-# api = "AIzaSyAF_pGbsDW3ccFzWoj4zE1ksnbUIZf0W6w"
-palm.configure(api_key=api)
-model = {"model": "models/text-bison-001"}
+# api = os.getenv("MAKERSUITE_API_TOKEN") 
+api = "AIzaSyAF_pGbsDW3ccFzWoj4zE1ksnbUIZf0W6w"
+genai.configure(api_key=api)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = Flask(__name__)
 
@@ -22,8 +23,8 @@ def financial_QA():
 def makersuite():
     q = request.form.get("q")
     q += " Please limit response to 4-5 sentences"
-    r = palm.generate_text(prompt=q, **model)
-    return(render_template("makersuite.html",r=r.result))
+    r = model.generate_content(q)
+    return(render_template("makersuite.html",r=r.text))
 
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
@@ -33,10 +34,24 @@ def prediction():
 def joke():
     heads = random.randint(0,1)
     if heads:
-        r=palm.generate_text(prompt="Tell me a Singaporean Joke", **model)
+        r=model.generate_content("Tell me a Singaporean Joke")
     elif not heads:
-        r= palm.generate_text(prompt="Singapore financial news in paragraph form, 2 sentences", **model)
-    return render_template("index.html", joke=r.result)
+        r= model.generate_content("Singapore financial news in paragraph form, 2 sentences")
+    return render_template("index.html", joke=r.text)
+
+@app.route("/TM", methods=["GET", "POST"])
+def TM():
+    return(render_template("TM.html"))
+
+@app.route("/SA", methods=["GET", "POST"])
+def SA():
+    return(render_template("SA.html"))
+
+@app.route("/SAR", methods=["GET", "POST"])
+def SAR():
+    q = request.form.get("q")
+    r = TextBlob(q).sentiment
+    return(render_template("SAR.html", r=r))
 
 if __name__ == "__main__":
     app.run()
